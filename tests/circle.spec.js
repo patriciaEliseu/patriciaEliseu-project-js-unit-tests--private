680e9cda8153926dc8a73e19bafe3783ad81dd9b
Patricia Eliseu <patyeliseu4@gmail.com>
2022-03-11 12:24:42 -0300

    atualizar requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -54,7 +54,6 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     });
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     it('Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.', () => {
-        // não inclui a area pq já tinha feito na linha 53.
-        expect(circle(3)).toMatchObject({ radius: 3, circumference: 18.84 });
+        expect(circle(3)).toMatchObject({ radius: 3, area: 28.259999999999998, circumference: 18.84 });
     });
 });
